bddce77ff14fadded952f6b26d9a108898aa222b
Trey_Gleason <treygleason8@gmail.com>
2020-09-10 10:40:07 -0700

    created RPGStore repo and able to commit changes to code

--- RPGStoreSimulator/Program.cs
+++ RPGStoreSimulator/Program.cs
@@ -7,6 +7,7 @@ namespace RPGStoreSimulator
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello git");
         }
     }
 }
