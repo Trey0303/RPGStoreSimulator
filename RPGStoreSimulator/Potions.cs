dc78e4637d9c309def00abd971239116d320b91e
Trey_Gleason <treygleason8@gmail.com>
2020-09-10 12:05:09 -0700

    add player quit and inventory commands. also created player inventory using a list, array, and text file.

---
+++ RPGStoreSimulator/Potions.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RPGStoreSimulator
+{
+    class Potions: Items
+    {
+    }
+}
--- RPGStoreSimulator/Program.cs
+++ RPGStoreSimulator/Program.cs
@@ -1,13 +1,65 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 
 namespace RPGStoreSimulator
 {
     class Program
     {
+        
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Hello git");
+            
+
+            //set Gamerunning to equal true
+            bool GameRunning = true;
+
+            //add text file for player inventory
+            string filePath = @"C:\Users\treyp\source\repos\RPGStoreSimulator\RPGStoreSimulator\Inventory.txt";
+            //string[] lines = File.ReadAllLines(filePath);
+            List<string> lines = new List<string>();
+            lines = File.ReadAllLines(filePath).ToList();
+
+            //game will continue to run until GameRunning turns false
+            while (GameRunning)
+            {
+                //ask for player input
+                Console.WriteLine("what would you like to do?");
+                //reads what player inputs
+                String playerCommand = Console.ReadLine();
+                //checks to see if player command matches any command on list
+                String[] commandArray = { "quit", "inv", "show inv", "show inventory", "inventory", "store" , "shop"};
+                //array used to convert player command into an int
+                int[] numberForm = { 0, 1, 1, 1, 1, 2, 2};
+
+                int strToInt = -1;
+                for(int i = 0; i < commandArray.Length; i++)
+                {
+                    if(playerCommand == commandArray[i])
+                    {
+                        strToInt = numberForm[i];
+                    }
+                }
+                switch (strToInt)
+                {
+                    //if player types "quit" game will stop running
+                    case 0:
+                        GameRunning = false;
+                        break;
+                    //shows player inventory when asked
+                    case 1:
+                        foreach (String line in lines)
+                        {
+                            Console.WriteLine(line);
+                        }
+                        break;
+                    case 2:
+                        break;
+                }
+            }
+
         }
     }
 }
